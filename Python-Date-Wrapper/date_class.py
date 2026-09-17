f43f0a5f74e0f17c67fd01eaf1236fa730d45efc
jm underwood <jm.underwood@hotmail.com>
2026-09-16 20:42:41 -0400

    Started writing code, wrote the constructor and the properties

--- Python-Date-Wrapper/date_class.py
+++ Python-Date-Wrapper/date_class.py
@@ -5,3 +5,14 @@ class Date:
     def __init__(self, year: int = 1900, month: int = 1, day: int = 1) -> None:
         self.__date = date(year, month, day)
     
+@property
+def year(self) -> int:
+    return self.__date.year
+
+@property
+def month(self) -> int:
+    return self.__date.month
+
+@property
+def day(self) -> int:
+    return self.__date.day
