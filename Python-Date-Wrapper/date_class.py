346c2b900d962c3ea4c0ff9337eb1ca699dad2c3
jm underwood <jm.underwood@hotmail.com>
2026-09-17 00:05:24 -0400

    Tested if the default constructor values worked by running the Test Explorer. Initially had issues due to having the properties placed before the methods.  I used copilot to identify that issue.

--- Python-Date-Wrapper/date_class.py
+++ Python-Date-Wrapper/date_class.py
@@ -1,22 +1,25 @@
-from datetime imoprt date
+from datetime import date
 import calendar
 
 class Date:
     def __init__(self, year: int = 1900, month: int = 1, day: int = 1) -> None:
         self.__date = date(year, month, day)
 
+    @property
+    def year(self) -> int:
+        return self.__date.year
+
+    @property
+    def month(self) -> int:
+        return self.__date.month
+
+    @property
+    def day(self) -> int:
+        return self.__date.day
+        
+        
     def set_date(self, year: int, month: int, day: int) -> None:
         new_date = date(year, month, day)
         self.__date = new_date
-    
-@property
-def year(self) -> int:
-    return self.__date.year
 
-@property
-def month(self) -> int:
-    return self.__date.month
 
-@property
-def day(self) -> int:
-    return self.__date.day
--- Python-Date-Wrapper/test_date_class.py
+++ Python-Date-Wrapper/test_date_class.py
@@ -1,1 +1,16 @@
+import unittest
+from date_class import Date
 
+class TestDate(unittest.TestCase):
+
+    def test_construction(self):
+        #Test if the constructor for the date class gives the desired  date
+        d = Date()
+        self.assertEqual(d.year, 1900)
+        self.assertEqual(d.month, 1)
+        self.assertEqual(d.day, 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
