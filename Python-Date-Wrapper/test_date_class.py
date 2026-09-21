b54ed15f1d57cd4719c11e31600456d1bfd80f28
jm underwood <jm.underwood@hotmail.com>
2026-09-20 23:10:12 -0400

    Altered the test_invalid_set_date call by removing the Raise(valueError) lines of code

--- Python-Date-Wrapper/test_date_class.py
+++ Python-Date-Wrapper/test_date_class.py
@@ -26,7 +26,7 @@ class TestDate(unittest.TestCase):
         #Test if the constructor raises ValueError for invalid dates
         with self.assertRaises(ValueError):
             d = Date(2001, 13, 15)  
-       with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError):
             d1 = Date(2001, 0, 10)  
         with self.assertRaises(ValueError):
             d2 = Date(2001, 4, 31) 
@@ -46,8 +46,8 @@ class TestDate(unittest.TestCase):
 
     def test_set_date(self):
         #Test if the day property can be modified
-        self.assertRaises(ValueError)
-        d = Date(2020, 2, 29)
+        with self.assertRaises(ValueError): 
+            d = Date(2020, 2, 29)
         d.set_date(2021, 2, 28)
         self.assertEqual(d.year, 2021)
         self.assertEqual(d.month, 2)
@@ -55,12 +55,11 @@ class TestDate(unittest.TestCase):
 
     def test_invalid_set_date(self):
         #Test if the day property can be modified
-        self.assertRaises(ValueError)
         d = Date(2020, 2, 29)
-        d.set_date(2021, 13, 28)
-        self.assertEqual(d.year, 2021)
-        self.assertEqual(d.month, 13)
-        self.assertEqual(d.day, 28)
+        d.set_date(2021, 13, 29)
+        self.assertEqual(d.year, 2020)
+        self.assertEqual(d.month, 2)
+        self.assertEqual(d.day, 29)
 
     def test_original_date_after_failed_change(self):
         #Test if the day property can be modified
