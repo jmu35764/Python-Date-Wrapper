0dcb62a95b1bd060012f6141136849b492bfc4e1
jm underwood <jm.underwood@hotmail.com>
2026-09-22 22:59:20 -0400

    Added a test for negative difference in test_sub_overload

--- Python-Date-Wrapper/test_date_class.py
+++ Python-Date-Wrapper/test_date_class.py
@@ -110,9 +110,11 @@ class TestDate(unittest.TestCase):
         #Test if the __sub__ method works correctly
         d1 = Date(2020, 4, 10)
         d2 = Date(2020, 4, 9)
-        result = d1 - d2
-        self.assertIsInstance(result, int)
-        self.assertEqual(result, 1)
+        result_1 = d1 - d2
+        result_2 = d2 - d1
+        self.assertIsInstance(result_1, int)
+        self.assertEqual(result_1, 1)
+        self.assertEqual(result_2, -1)
 
 
 if __name__ == '__main__':
