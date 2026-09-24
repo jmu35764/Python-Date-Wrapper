cee18a9dc6098811affde275a7a9f8aa67ee1fd4
jm underwood <jm.underwood@hotmail.com>
2026-09-23 23:05:44 -0400

    test_date_class < test_string_overload: Added validation functions

--- Python-Date-Wrapper/test_date_class.py
+++ Python-Date-Wrapper/test_date_class.py
@@ -150,7 +150,16 @@ class TestDate(unittest.TestCase):
         returned_value = test_date.decrement()
         self.assertIs(returned_value, test_date)
 
-        
+    def test_string_overload(self) -. str:
+        #Test if the __str__ method works correctly
+        d1 = Date(2018, 4, 18)
+        self.assertEqual(str(d1), "April 18, 2018")
+        d2 = Date(2014, 3, 1)
+        self.assertEqual(str(d2), "March 1, 2014")
+        d3 = Date(2016, 2, 29)
+        self.assertEqual(str(d3), "February 29, 2016")
+        d4 = Date(2014, 1, 1)
+        self.assertEqual(str(d4), "January 1, 2014")
 
 
 if __name__ == '__main__':
