4435718aca8e6a7a867c236c9beb5f55396bdb13
jm underwood <jm.underwood@hotmail.com>
2026-09-23 20:49:42 -0400

    Added d4, a fourth date object that contains a different year from d3, and a new assertEqual function that subtracts dates of different years.

--- Python-Date-Wrapper/test_date_class.py
+++ Python-Date-Wrapper/test_date_class.py
@@ -111,12 +111,14 @@ class TestDate(unittest.TestCase):
         d1 = Date(2020, 4, 10)
         d2 = Date(2020, 4, 9)
         d3 = Date(2020, 5, 9)
+        d4 = Date(2021, 5, 9)
 
         self.assertIsInstance(d1-d2, int)
         self.assertEqual(d1-d2, 1)
         self.assertEqual(d2-d1, -1)
         self.assertEqual(d1-d1, 0)
-        self.assertEqual(d3-d2, 31)
+        self.assertEqual(d3-d2, 30)
+        self.assertEqual(d4-d3, 365)
 
 
 
