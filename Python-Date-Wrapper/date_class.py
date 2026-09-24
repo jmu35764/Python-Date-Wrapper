86f4f0e73af5f2146e6714de906dfd01121a02a3
jm underwood <jm.underwood@hotmail.com>
2026-09-24 00:17:17 -0400

    Added try: to from_input method

--- Python-Date-Wrapper/date_class.py
+++ Python-Date-Wrapper/date_class.py
@@ -93,10 +93,12 @@ class Date:
     def from_input(cls) -> "Date":
         '''Creates a Date object from user input.
         '''
-        year = int(input("Enter year (YYYY): "))
-        month = int(input("Enter month (1-12): "))
-        day = int(input("Enter day (1-31): "))
-        return cls(year, month, day)
+        try:
+            
+            year = int(input("Enter year (YYYY): "))
+            month = int(input("Enter month (1-12): "))
+            day = int(input("Enter day (1-31): "))
+            return cls(year, month, day)
 
         except ValueError as e:
             print(f"Invalid date: {e}. Please try again.")
