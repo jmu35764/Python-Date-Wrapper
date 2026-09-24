33747a635ae7119512c29821d3f45d5c32b99033
jm underwood <jm.underwood@hotmail.com>
2026-09-23 21:19:25 -0400

    date_class: deleted Conv_value method

--- Python-Date-Wrapper/date_class.py
+++ Python-Date-Wrapper/date_class.py
@@ -66,11 +66,6 @@ class Date:
     #This comment is to test if the branch appears on github
 
     
-    def Conv_Value(self) -> int:
-        '''Returns the date as an integer value counting the number of days since a reference date.
-        '''
-        return self.__date.toordinal()
-
     def __sub__(self, other: object) -> int:
         '''Returns the difference in days between two Date objects.
         '''
